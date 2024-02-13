628740418532c702c6b5ab94ae9a112e474fee79
CabaCrD <147181926+CabaCrD@users.noreply.github.com>
2024-02-13 04:15:08 +0100

    Update listado.php

--- Crud-basico-tiendaweb/listado.php
+++ Crud-basico-tiendaweb/listado.php
@@ -32,18 +32,25 @@
         <div class="container mt-1 mb-3">
             <div class="col-12 d-flex border border-dark bg-secondary text-light">
                 <div class="col-md-12 text-center justify-content-center">
-                    <h1>Tarea</h1>
-                    <h2>Listado de productos de una familia</h2>
+                    <h1>Tienda Web</h1>
+
+                    <form class="col-md-12 col-sm-6 w-100" method="POST">
+                        <input type="text" id="buscador" name="buscador" placeholder="Buscar un producto" class="col-md-10">
+                        <input type="submit" value="Buscar" id="btnBuscar" name="btnBuscar" class="col-md-1 col-sm-1 btn btn-primary mb-2 mt-2">
+                    </form>
+
                     <form class="col-md-12 col-sm-6 w-100" method="POST">
                         <label for="combo" class="col-md-1 col-sm-2">Familias: </label>
                         <select id="combo" name="combo" class="form-select col-md-3 col-sm-3" required>
                             <option selected disabled >Seleccione una familia</option>
+
                                 <?php
                                     $combo = controller::getFamilias();//LLAMAMOS AL METODO PARA LLAMAR A LAS FAMILIAS
                                     for ($i = 0; $i < count($combo); $i++) {//USAMOS EL BUCLE PARA RELLENAR EL COMBO
                                         echo "<option value='" . $combo[$i]["cod"] . "'>" . $combo[$i]["nombre"] . "</option>";//COPN ESTO RELLENAREMOS LAS OPCIONES
                                     }
 	                            ?>
+
                         </select>
                         <input type="submit" value="Mostrar" id="btnEnv" name="btnEnv" class="col-md-1 col-sm-1 btn btn-primary mb-2 mt-2">
                     </form>
@@ -56,13 +63,38 @@
         <div class="container">
             <div class="col-12 d-flex">
                 <div class="col-md-12 text-center justify-content-center">
-                    <h3>Productos de la familia</h3>
+                    <h3>Productos</h3>
                 </div>
             </div>            
         </div>
         <div class="container">
             <div class="col-12 d-flex">
                 <div class="col-md-12 text-left justify-content-start align-items-start ">
+
+                            <?php
+
+                                $busqueda = $_POST['buscador'];
+                                if(isset($_POST['btnBuscar'])){//SI SE ENVIO...
+                                    if(empty($busqueda)){
+                                        header("Refresh:0");
+                                    }
+                                    $buscador = controller::getBuscador($_POST["buscador"]);
+                                    echo "Resultados para: <b>" . $busqueda . "</b>";
+
+                                    for ($i = 0; $i < count($buscador); $i++) {
+
+                                        echo  "<form action='editar.php' method='POST' class='form-inline border bg-light mt-2 mb-2'>";
+                                        echo '<label for="editar">' . $buscador[$i]["nombre_corto"] . '&nbsp;</label>';             
+                                        echo '<input type="submit" value="Editar" id="btnEdit" name="btnEdit" class="col-md-1 col-sm-1 btn btn-success mb-2 mt-2">';
+                                        echo '<input type="hidden" value="' . $buscador[$i]["cod"] . '" id="cod" name="cod">';    
+                                        echo  "</form>";
+
+                                    }
+
+                                }
+                                
+	                        ?>
+
                         <?php
                             if(isset($_POST['btnEnv'])){//SI SE ENVIO...
                                 $prod= controller::getProductos($_POST['combo']);//LLAMAMOS A LA FUNCION Y LE PASAMOS LA VARIABLE QUE CONTIENE EL CODIGO DE LA FAMILIA          
